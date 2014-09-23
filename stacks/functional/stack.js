279d11a3081411953f5b80169925c00c399fdcff
Alex Hawkins <alexhawkins.me@gmail.com>
2014-09-23 16:50:59 -0700

    Implement functional inheritance Stack

---
+++ stacks/functional/stack.js
@@ -0,0 +1,41 @@
+//functional stack implementation
+
+var makeStack = function() {
+    //set local variables to use throughout
+    var length = 0,
+        storage = {},
+        stack = {
+
+            /******PROPERTIES******/
+
+            /*****METHODS*****/
+            push: function(value) {
+                storage[length] = value;
+                length++;
+            },
+
+            pop: function() {
+                if (length > 0) {
+                    var result = storage[length - 1];
+                    delete storage[length - 1];
+                    length--;
+                    return result;
+                }
+            },
+
+            size: function() {
+                return length;
+            },
+
+            storage: function() {
+                return storage;
+            }
+        };
+    return stack;
+
+};
+var stack = makeStack();
+stack.push(10);
+stack.push(20);
+console.log(stack.size()); //2
+console.log(stack.storage()); //{ '0': 10, '1': 20 }
